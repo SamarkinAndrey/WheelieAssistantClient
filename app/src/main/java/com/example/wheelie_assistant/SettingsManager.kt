6a7f5eeec14aefdd672fbaf44bf64815eadc8528
Andrey@mail.com <you@example.com>
2025-11-04 03:18:22 +0300

    1

--- app/src/main/java/com/example/wheelie_assistant/SettingsManager.kt
+++ app/src/main/java/com/example/wheelie_assistant/SettingsManager.kt
@@ -8,6 +8,8 @@ data class Settings(
     var system_tick: Int = 0,
     var prediction_horizont: Int = 0,
     var gyro_hysteresis: Float = 0f,
+    var reversed_pitch: Int = 0,
+    var reversed_roll: Int = 0,
     var target_pitch: Float = 0f,
     var dead_zone: Float = 0f,
     var exit_threshold: Float = 0f,
@@ -30,6 +32,8 @@ object SettingsManager {
                 system_tick = parser.getInt(B_SYSTEM_TICK, 0),
                 prediction_horizont = parser.getInt(B_PREDICTION_HORIZONT, 0),
                 gyro_hysteresis = parser.getFloat(B_GYRO_HYSTERESIS, 0f),
+                reversed_pitch = parser.getInt(B_REVERSED_PITCH, 0),
+                reversed_roll = parser.getInt(B_REVERSED_ROLL, 0),
                 target_pitch = parser.getFloat(B_TARGET_PITCH, 0f),
                 dead_zone = parser.getFloat(B_DEAD_ZONE, 0f),
                 exit_threshold = parser.getFloat(B_EXIT_THRESHOLD, 0f),
@@ -54,6 +58,8 @@ object SettingsManager {
             put(B_SYSTEM_TICK.s(), settings.system_tick)
             put(B_PREDICTION_HORIZONT.s(), settings.prediction_horizont)
             put(B_GYRO_HYSTERESIS.s(), settings.gyro_hysteresis)
+            put(B_REVERSED_PITCH.s(), settings.reversed_pitch)
+            put(B_REVERSED_ROLL.s(), settings.reversed_roll)
             put(B_TARGET_PITCH.s(), settings.target_pitch)
             put(B_DEAD_ZONE.s(), settings.dead_zone)
             put(B_EXIT_THRESHOLD.s(), settings.exit_threshold)
@@ -72,6 +78,8 @@ object SettingsManager {
         return (settings.system_tick in 5..100) &&
             (settings.prediction_horizont in settings.system_tick..250) &&
             (settings.gyro_hysteresis in 0.0f..5.0f) &&
+            (settings.reversed_pitch in 0..1) &&
+            (settings.reversed_roll in 0..1) &&
             (settings.target_pitch in 15.0f..65.0f) &&
             (settings.dead_zone in 0.0f..5.0f) &&
             (settings.exit_threshold in 0.0f..10.0f) &&
