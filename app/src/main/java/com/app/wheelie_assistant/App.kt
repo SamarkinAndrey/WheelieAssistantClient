71770361b71f5d64da85859763f730f968551e62
Andrey@mail.com <you@example.com>
2025-12-16 13:53:02 +0300

    refactoring

--- app/src/main/java/com/app/wheelie_assistant/App.kt
+++ app/src/main/java/com/app/wheelie_assistant/App.kt
@@ -1,11 +1,21 @@
 package com.app.wheelie_assistant
 
 import android.app.Application
+import android.bluetooth.BluetoothDevice
 import java.lang.ref.WeakReference
 
 class App : Application() {
+
   companion object {
+
+    private var _mainActivityRef: WeakReference<MainActivity>? = null
     private var _settingsActivityRef: WeakReference<SettingsActivity>? = null
+
+    var mainActivity: MainActivity?
+      get() = _mainActivityRef?.get()
+      set(value) {
+        _mainActivityRef = if (value != null) WeakReference(value) else null
+      }
 
     var settingsActivity: SettingsActivity?
       get() = _settingsActivityRef?.get()
@@ -15,8 +25,16 @@ class App : Application() {
   }
 
   lateinit var bleManager: AppBleManager
+    private set
+
+  lateinit var scanManager: AppBleScanManager
+    private set
+
   lateinit var otaManager: AppOtaManager
+    private set
+
   lateinit var prefManager: AppPrefsManager
+    private set
 
   override fun onCreate() {
     super.onCreate()
@@ -24,5 +42,23 @@ class App : Application() {
     bleManager = AppBleManager(applicationContext)
     otaManager = AppOtaManager(applicationContext, bleManager)
     prefManager = AppPrefsManager(applicationContext)
- }
+    scanManager = AppBleScanManager(
+      applicationContext,
+      AppBleManager.SERVICE_UUID,
+      object : AppBleScanManager.ICallback {
+        override fun onDeviceFound(device: BluetoothDevice, rssi: Int) {
+          mainActivity?.onDeviceFound(device, rssi)
+        }
+        override fun onScanStarted() {
+          mainActivity?.onScanStarted()
+        }
+        override fun onScanFailed(errorCode: Int) {
+          mainActivity?.onScanFailed(errorCode)
+        }
+        override fun onScanStopped() {
+          mainActivity?.onScanStopped()
+        }
+      }
+    )
+  }
 }
